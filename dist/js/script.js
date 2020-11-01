621e30dd6019dac33d0cac3c99d508309a8d163f
Veronika <akulova.veronika17@gmail.com>
2020-11-01 21:16:58 +0700

    created section header and section 'subheader' for main page

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', ()=> {
+    $('.carousel').carousel({
+        interval: false,
+        ride: false,
+        touch: true,
+        keyboard: false,
+        wrap: true
+    });
+});
