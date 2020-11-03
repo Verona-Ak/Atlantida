e700a4f45eb68a2730a6c2fb56d57d1733fcda5d
Veronika <akulova.veronika17@gmail.com>
2020-11-03 14:51:18 +0700

    made section 'ourteam'

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,4 +7,20 @@ window.addEventListener('DOMContentLoaded', ()=> {
         keyboard: false,
         wrap: true
     });
+
+    // Расскрытие и скрытие информации о конкретном человеке в секции "ourteam"
+    let parent = document.querySelector('.ourteam__photo'),
+        item = document.querySelectorAll('.ourteam__item');
+
+    parent.addEventListener('click', function(e) {
+        for (let i = 0; i < item.length; i++) {
+            let img = item[i].querySelector('img'),
+                inf = item[i].querySelector('.ourteam__inf');
+            if(e.target && e.target == img || e.target == inf) {
+                let inf = item[i].querySelector('.ourteam__inf');
+                inf.classList.toggle('ourteam__inf_active');
+            }
+        }
+    });
+
 });
